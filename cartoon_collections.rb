e2c871c08bf1c63e5762c6a65548777e95a39420
Ethan Roberts <ejrobe@gmail.com>
2018-12-19 05:11:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  result = arr.find {|x| cheese_types.include?(x)}
+  arr.find {|x| cheese_types.include?(x)}
 end
